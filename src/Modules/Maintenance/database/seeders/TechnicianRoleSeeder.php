6e9f187a9c88b027c2ea153a23986ed55cfde614
HaroldF58 <harold_179@hotmail.com>
2026-05-28 23:34:37 -0400

    fix(maintenance): TechnicianRoleSeeder crea permiso isTechnician y lo asigna al rol

--- src/Modules/Maintenance/database/seeders/TechnicianRoleSeeder.php
+++ src/Modules/Maintenance/database/seeders/TechnicianRoleSeeder.php
@@ -3,15 +3,24 @@
 namespace Modules\Maintenance\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class TechnicianRoleSeeder extends Seeder
 {
     public function run(): void
     {
-        Role::firstOrCreate([
-            'name' => 'technician',
-            'guard_name' => 'web',
-        ]);
+        $role = Role::firstOrCreate(
+            ['name' => 'technician', 'guard_name' => 'web']
+        );
+
+        $permission = Permission::firstOrCreate(
+            ['name' => 'isTechnician', 'guard_name' => 'web'],
+            ['description' => 'Acceso a las funciones del módulo de mantenimiento']
+        );
+
+        $role->givePermissionTo($permission);
+
+        app()['cache']->forget('spatie.permission.cache');
     }
 }
